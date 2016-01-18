113ecc2d4c207afb28ca329c48752863ec431880
DAVID HEYDUCK <heyduckd@gmail.com>
2016-01-18 15:10:20 -0800

    resized images and started building my image constructor array and function.

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// This is where my javascript will go
+var imageChoices = document.getElementById('imageSelector');
+
+var imageConstructorArray = [];
+imageConstructorArray[0] = ['R2D2 Carry-On', 'img/bag.jpg', 'Take your own personal R2D2 around with you on your travels. This roller bag s TSA approved for carry-on size.', '$49.99'];
+imageConstructorArray[1] = ['Perfect Slice: Banana', 'img/banana.jpg', 'This contraption cuts your bananas in one easy motion. Great for feeding children or making fruit salads.', '$9.99'];
+imageConstructorArray[2] = ['Boot Boots', 'img/boots.jpg', 'Looking for the rainboot style but not the feet sweat that comes with it? Try these on for size. They are styled like a rain boot but have ventilation for maximum breathability.', '$59.99'];
+imageConstructorArray[3] = ['Bosu Chair', 'img/chair.jpg', 'Bosu balls have proven to increase core strength with certain excersizes. This chair allows you to sit and workout at the same time! Work on core strength while sitting at your computer, dinner table, or wherever you take this magnificent chair.', '$24.99'];
+imageConstructorArray[4] = ['Cthulhu Action Figure', 'img/cthulhu.jpg', 'Your child\'s next favorite toy! This action figure is made with recycled materials and guarantees satisfaction for hours.', '$12.99'];
+imageConstructorArray[5] = ['Dragon Meat', 'img/dragon.jpg', 'Gain strength and muscle mass by consuming this canned dragon meat. A great source of protein and magical dragon powers.', '$3.99'];
+imageConstructorArray[6] = ['Pen Cap Silverware', 'img/pen.jpg', 'Ultra compact eating utensils that attach to your writing aparatus! Never use your fingers to eat again.', '$2.99'];
+imageConstructorArray[7] = ['Pizza Scissors', 'img/scissors.jpg', 'This baby is the ultimate sidekick to eating pizza at home. Fuse your pizza cutter and your slice spatula into one and increase efficiency in the kitchen!', '$5.99'];
+imageConstructorArray[8] = ['Shark Sleeping Bag', 'img/shark.jpg', 'Sleep in the warmth of this new sleeping bag that looks like a shark! Also doubles as a halloween costume.', '$69;99'];
+imageConstructorArray[9] = ['Baby Sweeper Onezie', 'img/sweep.jpg', 'Your beloved baby is always crawling around, so why not put them to use! They will be cleannig your floors without even knowing it!', '$27.99'];
+imageConstructorArray[10] = ['Unicorn Meat', 'img/unicorn.jpg', 'Now you can fly with the mystical creatures of the universe. Eat this canned unicorn meat and you will start seeing these creatures all around you! Fly and run with them for 30 minutes after consumption.', '$3.99'];
+imageConstructorArray[11] = ['Gator Tail 128GB USB', 'img/usb.gif', 'Never run out of thumb drive storage with this massive 128GB USB storage device. When you plug it in, the tail wiggles like it\'s been freshly chopped off!', '$15.99'];
+imageConstructorArray[12] = ['Infinity Water Pot', 'img/water-can.jpg', 'This watering pot never runs out of water! Once you put water in, it will water itself for as long as your arm can hold it up! Great photo prop.', '$9.99'];
+imageConstructorArray[13] = ['Chug Wine Glass', 'img/wine-glass.jpg', 'Some people drink wine to be classy, but you drink wine to get drunk! This wine glass is made for you. Never spill when things get intense. Put your entire mouth around the opening and no liquid will be spilt!', '$7,99'];
+
+function imageConstructor() {
+
+}
